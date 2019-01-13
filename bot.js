1045e31f2a88f0eb1f852b5653b2e74641d13eff
Amr <44549704+WallBall22@users.noreply.github.com>
2019-01-13 18:25:28 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'hi') {
+    msg.reply('Welcome To Our Server :wave: :wave:');
+  }
+});
+
+client.login('NTM0MDQ1MTY0NTQ1NjM4NDQz.Dxz35g.psldIb9OfHJJiac0faJ2-OEzwJU');
